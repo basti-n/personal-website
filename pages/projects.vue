e380ac7697b40de576827e485391a2e07c6b85a6
Sebastian Neumair <sebastianneumair@Sebastians-MBP.fritz.box>
2019-10-19 23:08:03 +0200

    Add Projects  Page

---
+++ pages/projects.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        Projekte
+      </h1>
+      <p>Einen kleinen Überblick gibt es auf <a href="https://github.com/basti-n" target="_blank">Github</p>
+    </div>
+  </section>
+</template>
