a87b0a52f5207fa4c97e348835bcb745a153116d
Sebastian Neumair <sebastianneumair@Sebastians-MBP.fritz.box>
2019-10-20 08:56:47 +0200

    Add Contacts Page

---
+++ pages/contact.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">Kontaktiere mich</h1>
+      <p>Folge mir auf <a href="https://www.linkedin.com/in/sebastian-neumair/" target="_blank">LinkedIn</a></p>
+    </div>
+  </section>
+</template>
